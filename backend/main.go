f631ae388326b6fbd9131b84dda655d1e10c76f6
David Santa <david.santa@booking.com>
2025-05-26 12:36:46 +0300

    Added CORS module and fixed CORS errors

--- backend/main.go
+++ backend/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.Default())
 
 	r.GET("/api/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"status": "Vulkyra API is running!"})
